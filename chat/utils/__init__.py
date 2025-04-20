42372eb89a65321772b853412ff61885a8dacec4
gpr_27 <144049390+gpr-27@users.noreply.github.com>
2025-04-20 15:13:05 +0530

    Delete chat directory

--- chat/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .document_loader import load_folder_documents, load_single_pdf
-from .qa_chain import create_qa_chain
-
-__all__ = ['load_folder_documents', 'load_single_pdf', 'create_qa_chain']
